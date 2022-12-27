4a451edc4f74d36b21f1a7d3080f3c719c5a5b98
gabriel <gajsilva@gmail.com>
2022-12-26 22:16:28 -0300

    update

---
+++ src/ex1/Raio.java
@@ -0,0 +1,16 @@
+package ex1;
+
+public class Raio {
+    static double pi = 3.141592654;
+
+    private static double areaCirculo(double raio) {
+        double elevado = raio * raio;
+        return pi * elevado;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Valor da area do circulo é "+areaCirculo(3));
+    }
+
+}
+
