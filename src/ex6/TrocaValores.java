73666c774664d6bfb45bbc4a37d6068b1469b0c0
gabriel <gajsilva@gmail.com>
2023-01-02 16:45:30 -0300

    update

---
+++ src/ex6/TrocaValores.java
@@ -0,0 +1,14 @@
+package ex6;
+
+public class TrocaValores {
+    public static void main(String[] args) {
+        int x = 2;
+        int y = 1;
+        int troca;
+        System.out.println(" "+x+", "+y);
+        troca = x;
+        x = y;
+        y = troca;
+        System.out.println(" "+x+", "+y);
+    }
+}
